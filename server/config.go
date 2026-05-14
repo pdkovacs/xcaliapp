xcaliapp/server

[feat] handle multiple drawing stores -> drawing-repos

config.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"strconv"
+	"strings"
 )
 
-type drawingStoreType int
+type drawingStoreType string
 
 const (
-	LOCAL_GIT drawingStoreType = iota
-	GITLAB
-	S3
+	LOCAL_GIT drawingStoreType = "LOCAL_GIT"
+	GITLAB    drawingStoreType = "GITLAB"
+	S3        drawingStoreType = "S3"
 )
 
 type passwordCredentials struct {
@@ -20,19 +20,100 @@ type passwordCredentials struct {
 	Password string `json:"password"`
 }
 
+type drawingRepoConfig struct {
+	name      string
+	label     string
+	storeType drawingStoreType
+	root      string
+	path      string
+}
+
+type drawingReposConfigs map[string]drawingRepoConfig
+
 type options struct {
 	port            int
 	passwordCreds   []passwordCredentials
 	drawingStoreTyp drawingStoreType
-	drawingStoreKey string
 }
 
-func getDrawingStoreLocation() string {
-	envvar := os.Getenv("DRAWINGSTORE_LOCATION")
-	if len(envvar) > 0 {
-		return envvar
+const (
+	drawingRepoEnvvarNameAppPart         = "XCALIAPP"
+	drawingRepoEnvvarNameFeaturePart     = "DRAWINGREPO"
+	drawingRepoEnvvarNameListPart        = "LIST" // maps names to labels; sample value: "wsgw:WebSocket Gateway,xcaliapp:Xcalidraw App"
+	drawingRepoEnvvarNameStorageTypePart = "STORETYPE"
+	drawingRepoEnvvarNameRootPart        = "ROOT"
+	drawingRepoEnvvarNamePathPart        = "PATH"
+)
+
+func getDrawingRepos() (drawingReposConfigs, error) {
+	logger := CreateFunctionLogger(getLogger(), "getDrawingRepos")
+
+	nameToConfigMap := drawingReposConfigs{}
+
+	envVarFeatureBaseName := drawingRepoEnvvarNameAppPart + "_" + drawingRepoEnvvarNameFeaturePart
+
+	listEnvVarName := envVarFeatureBaseName + "_" + drawingRepoEnvvarNameListPart
+	for _, env := range os.Environ() {
+		envNameAndValue := strings.Split(env, "=")
+		envName := envNameAndValue[0]
+		envValue := envNameAndValue[1]
+		if envName == listEnvVarName {
+			namesAndLabels := strings.SplitSeq(envValue, ",") // e.g. "WebSocket Gateway:wsgw"
+			for nameAndLabel := range namesAndLabels {
+				nl := strings.Split(nameAndLabel, ":")
+				nameToConfigMap[nl[0]] = drawingRepoConfig{
+					name:  nl[0],
+					label: nl[1],
+				}
+			}
+		}
 	}
-	return filepath.Join(os.Getenv("HOME"), ".xcaliapp", "drawing-store")
+
+	setupConfigForName := func(config drawingRepoConfig) drawingRepoConfig {
+		for _, env := range os.Environ() {
+			envVarNamePrefix := envVarFeatureBaseName + "_" + config.name + "_"
+			envNameAndValue := strings.Split(env, "=")
+			envName := envNameAndValue[0]
+			envValue := envNameAndValue[1]
+			if len(envName) > len(envVarNamePrefix) && envName[0:len(envVarNamePrefix)] == envVarNamePrefix {
+				property := envName[len(envVarNamePrefix):]
+				switch property {
+				case drawingRepoEnvvarNameStorageTypePart:
+					config.storeType = drawingStoreType(envValue)
+				case drawingRepoEnvvarNameRootPart:
+					config.root = envValue
+				case drawingRepoEnvvarNamePathPart:
+					config.path = envValue
+				}
+
+				if envName == envVarNamePrefix+"_"+drawingRepoEnvvarNameStorageTypePart {
+					config.storeType = drawingStoreType(envValue)
+				}
+				if envName == envVarNamePrefix+"_"+drawingRepoEnvvarNameRootPart {
+					config.root = envValue
+				}
+				if envName == envVarNamePrefix+"_"+drawingRepoEnvvarNamePathPart {
+					config.path = envValue
+				}
+			}
+		}
+		return config
+	}
+
+	for _, config := range nameToConfigMap {
+		nameToConfigMap[config.name] = setupConfigForName(config)
+	}
+
+	for _, value := range nameToConfigMap {
+		if value.root == "" {
+			logger.Warn().Interface("drawingRepoConfig", value).Msg("No root")
+		}
+		if value.path == "" {
+			logger.Info().Interface("drawingRepoConfig", value).Msg("No path")
+		}
+	}
+
+	return nameToConfigMap, nil
 }
 
 const DefaultServerPort = 8080

config_test.go
@@ -1,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type readConfigurationTestSuite struct {
+	suite.Suite
+}
+
+func TestReadConfiguration(t *testing.T) {
+	suite.Run(t, &readConfigurationTestSuite{})
+}
+
+func (t *readConfigurationTestSuite) TestGetDrawingRepoConfig() {
+	wsgwName := "wsgw"
+	wsgwLabel := "WebSocket Gateway"
+	wsgwRoot := "wsgw-root"
+	wsgwPath := "wsgw_path"
+	xcaliName := "xcali"
+	xcaliLabel := "XCalidraw Application"
+	xcaliRoot := "xcali-root"
+	xcaliPath := "xcali_path"
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_LIST", fmt.Sprintf("%s:%s,%s:%s", wsgwName, wsgwLabel, xcaliName, xcaliLabel))
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_wsgw_ROOT", wsgwRoot)
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_wsgw_PATH", wsgwPath)
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_xcali_ROOT", xcaliRoot)
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_xcali_PATH", xcaliPath)
+
+	expectedSets := drawingReposConfigs{
+		wsgwName: drawingRepoConfig{
+			name:  wsgwName,
+			label: wsgwLabel,
+			root:  wsgwRoot,
+			path:  wsgwPath,
+		},
+		xcaliName: drawingRepoConfig{
+			name:  xcaliName,
+			label: xcaliLabel,
+			root:  xcaliRoot,
+			path:  xcaliPath,
+		},
+	}
+
+	drawingRepos, err := getDrawingRepos()
+
+	t.NoError(err)
+	t.Equal(expectedSets, drawingRepos)
+}

pdkovacs/xcaliapp

server/config.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"strconv"
+	"strings"
 )
 
-type drawingStoreType int
+type drawingStoreType string
 
 const (
-	LOCAL_GIT drawingStoreType = iota
-	GITLAB
-	S3
+	LOCAL_GIT drawingStoreType = "LOCAL_GIT"
+	GITLAB    drawingStoreType = "GITLAB"
+	S3        drawingStoreType = "S3"
 )
 
 type passwordCredentials struct {
@@ -20,19 +20,100 @@ type passwordCredentials struct {
 	Password string `json:"password"`
 }
 
+type drawingRepoConfig struct {
+	name      string
+	label     string
+	storeType drawingStoreType
+	root      string
+	path      string
+}
+
+type drawingReposConfigs map[string]drawingRepoConfig
+
 type options struct {
 	port            int
 	passwordCreds   []passwordCredentials
 	drawingStoreTyp drawingStoreType
-	drawingStoreKey string
 }
 
-func getDrawingStoreLocation() string {
-	envvar := os.Getenv("DRAWINGSTORE_LOCATION")
-	if len(envvar) > 0 {
-		return envvar
+const (
+	drawingRepoEnvvarNameAppPart         = "XCALIAPP"
+	drawingRepoEnvvarNameFeaturePart     = "DRAWINGREPO"
+	drawingRepoEnvvarNameListPart        = "LIST" // maps names to labels; sample value: "wsgw:WebSocket Gateway,xcaliapp:Xcalidraw App"
+	drawingRepoEnvvarNameStorageTypePart = "STORETYPE"
+	drawingRepoEnvvarNameRootPart        = "ROOT"
+	drawingRepoEnvvarNamePathPart        = "PATH"
+)
+
+func getDrawingRepoConfigs() (drawingReposConfigs, error) {
+	logger := CreateFunctionLogger(getLogger(), "getDrawingRepos")
+
+	nameToConfigMap := drawingReposConfigs{}
+
+	envVarFeatureBaseName := drawingRepoEnvvarNameAppPart + "_" + drawingRepoEnvvarNameFeaturePart
+
+	listEnvVarName := envVarFeatureBaseName + "_" + drawingRepoEnvvarNameListPart
+	for _, env := range os.Environ() {
+		envNameAndValue := strings.Split(env, "=")
+		envName := envNameAndValue[0]
+		envValue := envNameAndValue[1]
+		if envName == listEnvVarName {
+			namesAndLabels := strings.SplitSeq(envValue, ",") // e.g. "WebSocket Gateway:wsgw"
+			for nameAndLabel := range namesAndLabels {
+				nl := strings.Split(nameAndLabel, ":")
+				nameToConfigMap[nl[0]] = drawingRepoConfig{
+					name:  nl[0],
+					label: nl[1],
+				}
+			}
+		}
 	}
-	return filepath.Join(os.Getenv("HOME"), ".xcaliapp", "drawing-store")
+
+	setupConfigForName := func(config drawingRepoConfig) drawingRepoConfig {
+		for _, env := range os.Environ() {
+			envVarNamePrefix := envVarFeatureBaseName + "_" + config.name + "_"
+			envNameAndValue := strings.Split(env, "=")
+			envName := envNameAndValue[0]
+			envValue := envNameAndValue[1]
+			if len(envName) > len(envVarNamePrefix) && envName[0:len(envVarNamePrefix)] == envVarNamePrefix {
+				property := envName[len(envVarNamePrefix):]
+				switch property {
+				case drawingRepoEnvvarNameStorageTypePart:
+					config.storeType = drawingStoreType(envValue)
+				case drawingRepoEnvvarNameRootPart:
+					config.root = envValue
+				case drawingRepoEnvvarNamePathPart:
+					config.path = envValue
+				}
+
+				if envName == envVarNamePrefix+"_"+drawingRepoEnvvarNameStorageTypePart {
+					config.storeType = drawingStoreType(envValue)
+				}
+				if envName == envVarNamePrefix+"_"+drawingRepoEnvvarNameRootPart {
+					config.root = envValue
+				}
+				if envName == envVarNamePrefix+"_"+drawingRepoEnvvarNamePathPart {
+					config.path = envValue
+				}
+			}
+		}
+		return config
+	}
+
+	for _, config := range nameToConfigMap {
+		nameToConfigMap[config.name] = setupConfigForName(config)
+	}
+
+	for _, value := range nameToConfigMap {
+		if value.root == "" {
+			logger.Warn().Interface("drawingRepoConfig", value).Msg("No root")
+		}
+		if value.path == "" {
+			logger.Info().Interface("drawingRepoConfig", value).Msg("No path")
+		}
+	}
+
+	return nameToConfigMap, nil
 }
 
 const DefaultServerPort = 8080

server/config_test.go
@@ -1,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type readConfigurationTestSuite struct {
+	suite.Suite
+}
+
+func TestReadConfiguration(t *testing.T) {
+	suite.Run(t, &readConfigurationTestSuite{})
+}
+
+func (t *readConfigurationTestSuite) TestGetDrawingRepoConfig() {
+	wsgwName := "wsgw"
+	wsgwLabel := "WebSocket Gateway"
+	wsgwRoot := "wsgw-root"
+	wsgwPath := "wsgw_path"
+	xcaliName := "xcali"
+	xcaliLabel := "XCalidraw Application"
+	xcaliRoot := "xcali-root"
+	xcaliPath := "xcali_path"
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_LIST", fmt.Sprintf("%s:%s,%s:%s", wsgwName, wsgwLabel, xcaliName, xcaliLabel))
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_wsgw_ROOT", wsgwRoot)
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_wsgw_PATH", wsgwPath)
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_xcali_ROOT", xcaliRoot)
+	t.T().Setenv("XCALIAPP_DRAWINGREPO_xcali_PATH", xcaliPath)
+
+	expectedSets := drawingReposConfigs{
+		wsgwName: drawingRepoConfig{
+			name:  wsgwName,
+			label: wsgwLabel,
+			root:  wsgwRoot,
+			path:  wsgwPath,
+		},
+		xcaliName: drawingRepoConfig{
+			name:  xcaliName,
+			label: xcaliLabel,
+			root:  xcaliRoot,
+			path:  xcaliPath,
+		},
+	}
+
+	drawingRepos, err := getDrawingRepoConfigs()
+
+	t.NoError(err)
+	t.Equal(expectedSets, drawingRepos)
+}